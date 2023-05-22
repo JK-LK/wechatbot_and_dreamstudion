787404ad63845ec1eec5c933d8bb4705274fc0e8
刘康 <liukang@liukangdeMacBook-Pro.local>
2023-05-22 15:55:17 +0800

    fix user_msg_hander bug

--- handlers/user_msg_handler.go
+++ handlers/user_msg_handler.go
@@ -17,21 +17,17 @@ var _ MessageHandlerInterface = (*UserMessageHandler)(nil)
 
 // UserMessageHandler 私聊消息处理
 type UserMessageHandler struct {
-	// 接收到消息
-	msg *openwechat.Message
-	// 发送的用户
-	sender *openwechat.User
 }
 
 // handle 处理消息
 func (g *UserMessageHandler) handle(msg *openwechat.Message) error {
 	cfg := config.LoadConfig()
 	//判断文本前缀是PictureToken，例如："生成图片"
-	if strings.HasPrefix(g.msg.Content, cfg.PictureToken) {
-		return g.ReplyImage()
+	if strings.HasPrefix(msg.Content, cfg.PictureToken) {
+		return g.ReplyImage(msg)
 	}
 	//如果是纯文本，使用ChatGPT进行回复
-	if g.msg.IsText() {
+	if msg.IsText() {
 		return g.ReplyText(msg)
 	}
 	return nil
@@ -71,24 +67,22 @@ func (g *UserMessageHandler) ReplyText(msg *openwechat.Message) error {
 	return err
 }
 
-func (g *UserMessageHandler) ReplyImage() error {
-	if time.Now().Unix()-g.msg.CreateTime > 60 {
+func (g *UserMessageHandler) ReplyImage(msg *openwechat.Message) error {
+	sender, err := msg.Sender()
+	if time.Now().Unix()-msg.CreateTime > 60 {
 		return nil
 	}
 	maxInt := rand.New(rand.NewSource(time.Now().UnixNano())).Intn(5)
 	time.Sleep(time.Duration(maxInt+1) * time.Second)
 
-	log.Printf("Received User[%v], Content[%v], CreateTime[%v]", g.sender.NickName, g.msg.Content,
-		time.Unix(g.msg.CreateTime, 0).Format("2006/01/02 15:04:05"))
+	log.Printf("Received User[%v], Content[%v], CreateTime[%v]", sender.NickName, msg.Content,
+		time.Unix(msg.CreateTime, 0).Format("2006/01/02 15:04:05"))
 
-	var (
-		replyPath string
-		err       error
-	)
+	var replyPath string
 
 	cfg := config.LoadConfig()
 	// 1.生成图片
-	text := strings.Replace(g.msg.Content, cfg.PictureToken, "", -1)
+	text := strings.Replace(msg.Content, cfg.PictureToken, "", -1)
 	replyPath, err = dreamstudio.TextToImage(text)
 
 	if err != nil {
@@ -96,7 +90,7 @@ func (g *UserMessageHandler) ReplyImage() error {
 		if strings.Contains(err.Error(), "context deadline exceeded") {
 			text = deadlineExceededText
 		}
-		_, err = g.msg.ReplyText(text)
+		_, err = msg.ReplyText(text)
 		if err != nil {
 			return fmt.Errorf("reply user error: %v ", err)
 		}
@@ -106,7 +100,7 @@ func (g *UserMessageHandler) ReplyImage() error {
 	//2.回复图片
 	img, _ := os.Open(replyPath)
 	defer img.Close()
-	_, err = g.msg.ReplyImage(img)
+	_, err = msg.ReplyImage(img)
 	if err != nil {
 		return fmt.Errorf("reply user error: %v ", err)
 	}
