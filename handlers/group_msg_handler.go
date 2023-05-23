df093a2d629896883445b697059c53504c419a7a
刘康 <liukang@liukangdeMacBook-Pro.local>
2023-05-23 14:51:32 +0800

    fix group bugs

--- handlers/group_msg_handler.go
+++ handlers/group_msg_handler.go
@@ -6,6 +6,7 @@ import (
 	"github.com/869413421/wechatbot/dreamstudio"
 	"github.com/869413421/wechatbot/gpt"
 	"github.com/eatmoreapple/openwechat"
+	"github.com/qingconglaixueit/wechatbot/service"
 	"github.com/sashabaranov/go-openai"
 	"log"
 	"math/rand"
@@ -21,10 +22,10 @@ type GroupMessageHandler struct {
 	self *openwechat.Self
 	// 群
 	group *openwechat.Group
-	// 接收到消息
-	msg *openwechat.Message
 	// 发送的用户
 	sender *openwechat.User
+	// 实现的用户业务
+	service service.UserServiceInterface
 }
 
 // handle 处理消息
@@ -33,7 +34,7 @@ func (g *GroupMessageHandler) handle(msg *openwechat.Message) error {
 	// 判断文本前缀是PictureToken，例如："生成图片"
 	if strings.Contains(msg.Content, cfg.PictureToken) {
 		log.Printf("开始生成图片%s", cfg.PictureToken)
-		return g.ReplyImage()
+		return g.ReplyImage(msg)
 	}
 	//如果是纯文本，使用ChatGPT进行回复
 	if msg.IsText() {
@@ -59,7 +60,7 @@ func (g *GroupMessageHandler) getRequestText(msg *openwechat.Message) []openai.C
 
 	// 2.替换掉当前用户名称
 	replaceText := "@" + g.self.NickName
-	requestText = strings.TrimSpace(strings.ReplaceAll(g.msg.Content, replaceText, ""))
+	requestText = strings.TrimSpace(strings.ReplaceAll(msg.Content, replaceText, ""))
 	if len(requestText) == 0 {
 		log.Println("user message is empty")
 		sessionText := make([]openai.ChatCompletionMessage, 0)
@@ -115,28 +116,28 @@ func (g *GroupMessageHandler) ReplyText(msg *openwechat.Message) error {
 	return err
 }
 
-func (g *GroupMessageHandler) ReplyImage() error {
-	if time.Now().Unix()-g.msg.CreateTime > 60 {
+func (g *GroupMessageHandler) ReplyImage(msg *openwechat.Message) error {
+	if time.Now().Unix()-msg.CreateTime > 60 {
 		return nil
 	}
 
 	maxInt := rand.New(rand.NewSource(time.Now().UnixNano())).Intn(5)
 	time.Sleep(time.Duration(maxInt+1) * time.Second)
 
-	log.Printf("Received Group[%v], Content[%v], CreateTime[%v]", g.group.NickName, g.msg.Content,
-		time.Unix(g.msg.CreateTime, 0).Format("2006/01/02 15:04:05"))
+	log.Printf("Received Group[%v], Content[%v], CreateTime[%v]", g.group.NickName, msg.Content,
+		time.Unix(msg.CreateTime, 0).Format("2006/01/02 15:04:05"))
 
 	var (
 		replyPath string
 		err       error
 	)
 	// 1.不是@的不处理
-	if !g.msg.IsAt() {
+	if !msg.IsAt() {
 		return nil
 	}
 	// 2.整理数据
 	cfg := config.LoadConfig()
-	text := strings.ReplaceAll(g.msg.Content, cfg.PictureToken, "")
+	text := strings.ReplaceAll(msg.Content, cfg.PictureToken, "")
 	replaceText := "@" + g.self.NickName
 	text = strings.ReplaceAll(text, replaceText, "")
 	if text == "" {
@@ -150,7 +151,7 @@ func (g *GroupMessageHandler) ReplyImage() error {
 		if strings.Contains(err.Error(), "context deadline exceeded") {
 			text = deadlineExceededText
 		}
-		_, err = g.msg.ReplyText(text)
+		_, err = msg.ReplyText(text)
 		if err != nil {
 			return fmt.Errorf("reply user error: %v ", err)
 		}
@@ -160,7 +161,7 @@ func (g *GroupMessageHandler) ReplyImage() error {
 	// 4.回复图片
 	img, _ := os.Open(replyPath)
 	defer img.Close()
-	_, err = g.msg.ReplyImage(img)
+	_, err = msg.ReplyImage(img)
 	if err != nil {
 		return fmt.Errorf("reply user error: %v ", err)
 	}
